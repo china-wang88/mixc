7f2d432975506038112b569a44a5d4d062cfa19a
LAPTOP-78SDSSI5\20897 <w2089704505@163.com>
2025-06-12 15:18:11 +0800

    第一个composer包

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+include "vendor/autoload.php";
+new \W14797034489\Mixc\demo();
---
+++ src/demo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace W14797034489\Mixc;
+include "./vendor/autoload.php";
+
+class demo
+{
+    public function __construct()
+    {
+        echo "12312";
+    }
+}
